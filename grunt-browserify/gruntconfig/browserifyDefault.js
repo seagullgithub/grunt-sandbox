7227eed353ba12a894d7f6964b3158fd19435aeb
seagullgithub <none>
2019-04-14 13:35:40 +0200

    added grunt-browserify

---
+++ grunt-browserify/gruntconfig/browserifyDefault.js
@@ -0,0 +1,23 @@
+'use strict';
+var grunt = require('grunt')
+
+module.exports = {
+
+options: {
+    debug: true,
+  },
+  dev: {
+    options: {
+    },
+    src: ['src/js/main.js'],
+    dest: 'build/assets/js/bundle.js'
+  },
+  production: {
+    options: {
+      debug: false
+    },
+    src: ['src/js/main.js'],
+    dest: 'build/assets/js/bundle.js'
+  }
+
+}
